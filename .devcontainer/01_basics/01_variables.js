0be406b35c232da9c4096b27adbb192f262c2d64
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-02-24 19:47:46 +0000

    Code Configuration done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="devansh@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+
+// accountId=2 // not allowed constant can't changes
+
+accountEmail="hc@hc.com"
+accountPassword="2121212"
+accountCity="Uttar Pradesh"
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity])
