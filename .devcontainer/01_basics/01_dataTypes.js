31b9774eaa32500978bb3db9803e5009e54e5578
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-02-25 17:04:07 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version of JS
+
+//alert(3+3)  // we are using nodejs, not browser
+
+console.log(3 // code readability should be high
+    +3) 
+
+    console.log("Devansh");
+
+    let name="devansh" // string data type
+    let age=18 // number data type
+
+    let isLoggedIn=false // boolean data type
+
+    let state=null; // null ko hum assign kar sakte hain
+
+    // Primitive Data Types
+
+    //1.number data type range=> 2^53(2 to the power 53)
+    //2.bigint
+    //3.string=> used in "" or '' but prefably used in double quotes
+    //4.boolean=> true or false
+    //5.null=> standalone value (null is basically an object it's type is object)
+    //6.undefined=> abhi value assign nhi hui hai
+    //7.symbol=> for uniqueness
+
+     
+    // object
+
+    console.log(typeof undefined); // it's type is undefined 
+    console.log(typeof null); // it's type is object
