55cedade8df5a644cae4802bc1612ef3e3900cef
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-02-27 17:21:19 +0000

    conversion Operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,28 +1,66 @@
-//let score = "33abc" => it's output is NaN(Not a Number)
-//let score = null    => // it's output is 0
-//let score = undefined  => it's output is NaN(Not a Number)
-//let score = false =>  it's output is 1 or 0(1->true/0->false)
-//let score="devansh" => it's output is NaN(Not a Number)
+//let score = "33abc" //=> it's output is NaN(Not a Number)
+//let score = null    //=>  it's output is 0
+//let score = undefined  //=> it's output is NaN(Not a Number)
+//let score = false  //=>  it's output is 1 or 0(1->true/0->false)
+//let score="devansh" //=> it's output is NaN(Not a Number)
 
-// console.log(typeof score);
-// console.log(typeof(score));
+//console.log(typeof score);
+ //console.log(typeof(score));
 
-// let valueInNumber=Number(score) 
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber); 
+ //let valueInNumber=Number(score) 
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber); 
 
-//let isLoggedIn = 1 => it's output is true
-//let isLoggedIn = "" => it's output is false because it is empty string
-//let isLoggedIn ="devansh" => it's output is true because it's not the empty string
-// let booleanisLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanisLoggedIn);
-// console.log( typeof booleanisLoggedIn);
+//let isLoggedIn = 1 //=> it's output is true
+//let isLoggedIn = "" //=> it's output is false because it is empty string
+//let isLoggedIn ="devansh" //=> it's output is true because it's not the empty string
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanisLoggedIn);
+ //console.log( typeof booleanisLoggedIn);
 
-let someNumber=33 // it's output is 33 and this number 33 is now converted into string
+ let someNumber=33 // it's output is 33 and this number 33 is now converted into string
 
-let stringNumber= String(someNumber)
+ let stringNumber= String(someNumber)
 
-console.log(stringNumber);
-console.log( typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log( typeof stringNumber);
 
 
+//*****************************Operations********************************//
+
+//  let value=3
+//  let negValue = -value
+
+//  console.log(negValue);
+
+//  //Basic Math Operations
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3); // 2^2(2 raise to the power)
+//  console.log(2/3);  // Divide
+//  console.log(2%2);  // Modulus(Remainder)
+
+
+ let str1 ="hello"
+ let str2 =" devansh"
+ let str3=str1+str2
+ //console.log(str3);
+
+//  console.log("1"+2); // if string appears first then the whole number is converted to string like the above example(output is 12)
+//  console.log(1+"2"); // output is 12
+//  console.log("1"+2+2);// output is 122
+//  console.log(1+2+"2"); //if two numbers appears first then first add these numbers then  converted to string like the above example(output is 1+2=3 then merge with 2 so output is 32)
+
+//  console.log((3+4)*5%3);
+
+// console.log(true); // it gives true
+// console.log(+true);// it gives 1
+// console.log(true+); // it gives error
+
+console.log(+false); // it gives 0
+console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3=2 + 2
