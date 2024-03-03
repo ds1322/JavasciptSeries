521bf26503c5189eaa05e3c51efad85932eece27
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-03-03 17:05:11 +0000

    DataTypes-Summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+//Primitive Data Types
+
+// 7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+
+const score=100
+const scoreValue=100.3
+//The above two defined variables is in Number Type
+
+const isLoggedIn = false
+const outsideTemp= null // null means empty not zero value
+
+let userEmail; // If we can't defined any value then it is undefined
+
+
+
+
+// Reference(Non-Primitive Data Types)
+
+//Array,Objects,Functions
