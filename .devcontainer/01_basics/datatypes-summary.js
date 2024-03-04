a14200e1eafe9baec5b9e0af68c49d56786d22b0
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-03-04 18:40:52 +0000

    dataTypes-Stack-heap

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -6,64 +6,91 @@
 //JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
 
 
-const score=100
-const scoreValue=100.3
-//The above two defined variables is in Number Type
+// const score=100
+// const scoreValue=100.3
+// //The above two defined variables is in Number Type
 
-const isLoggedIn = false
-const outsideTemp= null // null means empty not zero value
+// const isLoggedIn = false
+// const outsideTemp= null // null means empty not zero value
 
-let userEmail; // If we can't defined any value then it is undefined
+// let userEmail; // If we can't defined any value then it is undefined
 
-let userData= undefined; // We can manually declared undefined also
+// let userData= undefined; // We can manually declared undefined also
 
-const id = Symbol('123')
-const anotherId=Symbol('123')
+// const id = Symbol('123')
+// const anotherId=Symbol('123')
 
-console.log(id===anotherId);
+// console.log(id===anotherId);
 
-const bigNumber = 345622292925266355353n 
-
-
+// const bigNumber = 345622292925266355353n 
 
 
-// Reference(Non-Primitive Data Types)
-//All Non-Primitive return types would be Object
 
-//Array,Objects,Functions
 
-// Array
-const heros = ["shaktiman", "naagraj","doga"]; // Reference Type or Non- Primitive Type
+// // Reference(Non-Primitive Data Types)
+// //All Non-Primitive return types would be Object
 
-// Objects
-//Objects defined in curlyBraces
-let myObj = {
-    name:"devansh",
-    age: 22,
+// //Array,Objects,Functions
 
-}
+// // Array
+// const heros = ["shaktiman", "naagraj","doga"]; // Reference Type or Non- Primitive Type
+
+// // Objects
+// //Objects defined in curlyBraces
+// let myObj = {
+//     name:"devansh",
+//     age: 22,
+
+// }
 
 //Functions
 
-const myFunction =  function(){ //It's type is object Function
+// const myFunction =  function(){ //It's type is object Function
 
-    console.log("Hello World");
+//     console.log("Hello World");
 
-}
+//}
 
 //How to find dataType of any value
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
-console.log(typeof outsideTemp);
+// console.log(typeof outsideTemp);
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
-console.log(typeof heros);
+// console.log(typeof heros);
 
-console.log(typeof myObj);
+// console.log(typeof myObj);
 
 
 //******************************************************************************* */
 
-//Stack Memory(Primitive Types) and Heap Memory(Non-Primitive Types)
+//1)Stack Memory(Primitive Types)=>Jab bhi stack memeory use hoti hai toh jo bhi humne variable declare kiya hai hume uski copy milti hai.
 
+// 2)Heap Memory(Non-Primitive Types)=> Jab bhi memeory heap ke andar define hoti hai toh uska hume reference(original value ka) diya jata hai, toh jo bhi hum change karenge wo original value me change hoga.
+
+let myYoutubename="devanshsharmadotcom"
+
+let anothername= myYoutubename
+
+anothername="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne={
+
+    email: "user@google.com",
+    upi: "user@ybl",
+
+
+}
+
+
+let userTwo = userOne
+
+userTwo.email="devansh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
