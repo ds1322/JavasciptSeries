6f6fb1a4c636217cf1e540fe6ec4bb543fbb42d7
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-03-08 20:50:44 +0000

    Strings

--- .devcontainer/01_basics/05_strings.js
+++ .devcontainer/01_basics/05_strings.js
@@ -3,18 +3,26 @@ const repoCount = 50
 
 //console.log(name+repoCount+" Value"); // This syntax is outdated
 
-console.log(`Hello my name is ${name} and my repo count is 
-${repoCount}`); // String Interpolation 
+//console.log(`Hello my name is ${name} and my repo count is 
+//${repoCount}`); // String Interpolation 
 
-const gameName = new String('devanshsh')
+const gameName = new String('devansh-sh')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toLowerCase());
-console.log(gameName.toUpperCase());
-console.log(gameName.lastIndexOf());
-console.log(gameName.toLocaleUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('s'));
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.lastIndexOf());
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-9,-6)
+
+console.log(anotherString);
+
