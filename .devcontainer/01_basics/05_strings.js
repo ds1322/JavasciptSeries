410747899a7f842ff42779349025efbba3083b05
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-03-08 20:09:34 +0000

    Strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "devansh"
+const repoCount = 50
+
+//console.log(name+repoCount+" Value"); // This syntax is outdated
+
+console.log(`Hello my name is ${name} and my repo count is 
+${repoCount}`); // String Interpolation 
+
+const gameName = new String('devanshsh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.lastIndexOf());
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
