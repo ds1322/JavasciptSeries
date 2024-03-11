4684e408f4d1f092e5b749fa6f3839b153a2638f
Devansh Sharma <80207138+ds1322@users.noreply.github.com>
2024-03-11 19:26:10 +0000

    Numbers and Maths

--- .devcontainer/01_basics/06_nums_and_maths.js
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -2,20 +2,37 @@ const score = 400
 
 //console.log(score);
 
-const balance = new Number(100)
+//const balance = new Number(100)
 
 //console.log(balance);
 
 //console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+//console.log(balance.toFixed(2));
 
-const otherNumber = 1123.8966
+//const otherNumber = 1123.8966
 
-console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(4));
 
-const hundreds = 1000000
+//const hundreds = 1000000
 
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
 
 //**********************Maths************************** */
 
+// console.log(Math);
+
+// console.log(Math.abs(-2));
+// console.log(Math.abs(-Infinity));
+// console.log(Math.round(4.66));
+// console.log(Math.ceil(3.6)); 
+// console.log(Math.floor(3.6));  
+// console.log(Math.min(4,3,6,8));  
+// console.log(Math.max(4,3,6,8));  
+
+
+//console.log(Math.floor(Math.random()*10) + 1);
+
+const min =10
+const max = 20
+
+console.log(Math.floor((Math.random()*(max-min +1)))+ min);
